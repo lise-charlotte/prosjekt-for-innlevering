45835255dfb3e1a1cfd19fb2432802209b3fd45c
lise-charlotte <lisecharlottekoder@gmail.com>
2024-12-16 13:58:58 +0100

    Trial and error

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-async function getApiData ()
+setTimeout(
+    function () {
+        console.log("Noe har skjedd");
+    },
+    5000
+)
+
+function delay(ms) {
+    return new Promise(function (resolve){
+        setTimeout(resolve, ms);
+    });
+}
+
+async function getApiData () {
+    const response = await fetch ("https://catfact.ninja/facts");
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+    console.log(data.data);
+    console.log(data.data[4]);
+    console.log(data.data[4].fact);
+
+    const catFact = data.data;
+    const factContainer = document.querySelector("#displayResult");
+
+
+    for (let index = 0; index < catFact.length; index++) {
+        console.log(catFact[index].fact);
+
+        const displayResult = document.createElement("displayResult");
+
+        const section = document.createElement("section");
+        section.appendChild("displayResult");
+
+    };
+
+    function generateFact() {
+        const randomIndex = Math.floor(Math.random() * catFact.length);
+        const randomFact = catFact[randomIndex];
+        document.querySelector("#displayResult") = randomFact
+
+    },
+};
+
+getApiData();
